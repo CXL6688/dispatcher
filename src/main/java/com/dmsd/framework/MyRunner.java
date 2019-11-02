d1a6ad6317edaaf56da07189656a04bcfc25530c
Caoxueliang <18333601856@163.com>
2019-11-02 09:48:20 +0800

    测试markdown语法

--- src/main/java/com/dmsd/framework/MyRunner.java
+++ src/main/java/com/dmsd/framework/MyRunner.java
@@ -1,9 +1,7 @@
 package com.dmsd.framework;
 
-import com.alibaba.fastjson.JSONObject;
 import com.dmsd.framework.dispatcher.DispatcherFactory;
 import com.dmsd.framework.dispatcher.IDispatcher;
-import com.dmsd.framework.handler.IHandler;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -16,13 +14,10 @@ public class MyRunner {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.scan("com.dmsd");
         ctx.refresh();
-        DispatcherFactory dispatcherFactory=ctx.getBean(DispatcherFactory.class);
-        /*IDispatcher<Integer> dispatcher= dispatcherFactory.createDispatcher("order",Integer.class);
-        Object result= dispatcher.doDispath(2);
-        System.out.println(result);*/
+        DispatcherFactory dispatcherFactory = ctx.getBean(DispatcherFactory.class);
 
-        IDispatcher<String> dispatcher= dispatcherFactory.createDispatcher("gender",String.class);
-        Object result= dispatcher.doDispath("男");
+        IDispatcher<String> dispatcher = dispatcherFactory.createDispatcher("gender", String.class);
+        Object result = dispatcher.doDispath("男");
         System.out.println(result);
     }
 }
--- src/main/java/com/dmsd/framework/dispatcher/DispatcherFactory.java
+++ src/main/java/com/dmsd/framework/dispatcher/DispatcherFactory.java
@@ -1,13 +1,21 @@
 package com.dmsd.framework.dispatcher;
 
 import org.springframework.stereotype.Component;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by CXL on 2019/11/1.
  */
 @Component
 public class DispatcherFactory {
+    private static Map<String,IDispatcher> map = new ConcurrentHashMap<>();
     public <T> IDispatcher  createDispatcher(String id,Class<T> clazz){
-        return new DefaultDispatcher<T>(id);
+        IDispatcher iDispatcher=map.get(id);
+        if(iDispatcher==null){
+            iDispatcher=new DefaultDispatcher<T>(id);
+            map.put(id,iDispatcher);
+        }
+        return iDispatcher;
     }
 }
